7e2c3ceb0df0a64867fb52ff62d7471fe584f5f8
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-12-13 22:09:13 -0600

    implement bmi calculator

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,19 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form @submit="handleSubmit">
+    <h1>BMI calculator</h1>
+
+    <div>
+      <label for="height">Height (KG)</label>
+      <input id="height" />
+    </div>
+
+    <div>
+      <label for="weight">Weight (M)</label>
+      <input id="weight" />
+    </div>
+
+    <button type="submit">Send</button>
+
+    <h2 v-if="bmi">Result: {{ bmi.toLocaleString() }}</h2>
+    <p v-if="bmiEstimation">Estimation: {{ bmiEstimation }}</p>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      bmi: null,
+      bmiEstimation: null,
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      const { height, weight } = e.target.elements;
+      const bmi = this.calculateBmi({
+        height: height.value,
+        weight: weight.value,
+      });
+      const bmiEstimation = this.calculateBmiEstimation({ bmi });
+
+      this.bmi = bmi;
+      this.bmiEstimation = bmiEstimation;
+    },
+    calculateBmi({ height, weight }) {
+      return weight / (height * height);
+    },
+    calculateBmiEstimation({ bmi }) {
+      if (bmi <= 18.5) {
+        return "Thinness";
+      }
+
+      if (bmi > 18.5 && bmi <= 24.9) {
+        return "Normal";
+      }
+
+      if (bmi >= 25 && bmi <= 29.9) {
+        return "Overweight";
+      }
+
+      if (bmi > -30) {
+        return "Obese";
+      }
+    },
+  },
+};
 </script>
 
 <style>
